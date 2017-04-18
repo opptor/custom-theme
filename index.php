875299848c83777d6fa4cc43bad543e7e66e6c87
Torsten Oppermann <torsten@sgalinski.de>
2017-04-18 08:50:19 +0200

    adding valid html tags

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
-		<h3><?php the_title(); ?></h3>
-		<div><?php the_content(); ?></div>
-	<?php endwhile;
-endif;
-?>
+<!doctype html>
+<html>
+	<head>
+		<title>Custom Theme Title</title>
+	</head>
+	<body>
+		<?php
+		if (have_posts()) :
+			while (have_posts()) : the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+				<div><?php the_content(); ?></div>
+			<?php endwhile;
+		endif;
+		?>
+	</body>
+</html>
+
+
